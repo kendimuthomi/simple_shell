aee6681f0193639684ec0b6bf8ba5895fc1db757
godima20 <godima20@gmail.com>
2022-08-08 15:22:08 +0000

    path.c

--- shell.h
+++ shell.h
@@ -104,8 +104,8 @@ void find_cmd(info_t *);
 void fork_cmd(info_t *);
 /* path.c */
 int is_cmd(info_t *, char *);
-char *dup_chars(char *, int, int);
-char *find_path(info_t *, char *, char *);
+char *_dup_chars(char *, int, int);
+char *find_path_(info_t *, char *, char *);
 /* loophsh.c */
 int loophsh(char **);
 /* err_string_functions.c */
