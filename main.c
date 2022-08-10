0b8f4379bc8eb77adfc6519ed460aaba4a143a34
godima20 <101834714+godima20@users.noreply.github.com>
2022-08-10 12:41:29 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,41 +1,27 @@
 #include "shell.h"
-/**
- * main - entry point
- * @ac: arg count
- * @av: arg vector
- *
- * Return: 0 on success and 1 on error
- */
-int main(int ac, char **av)
-{
-	info_t info[] = { INFO_INIT };
-	int fd = 2;
 
-	asm ("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=r" (fd)
-		:"r" (fd));
-	if (ac == 2)
-	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
-		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs_(av[0]);
-				_eputs_(": 0: Can't open ");
-				_eputs_(av[1]);
-				_eputchar_(BUF_FLUSH);
-				exit(127);
-			}
-		return (EXIT_FAILURE);
-		}
-		info->readfd = fd;
-	}
-	_populate_env_list(info);
-	read_history(info);
-	hsh(info, av);
-	return (EXIT_SUCCESS);
-}	
+/**
+ * main - Entry point of the program.
+ * @ac: The number of parameters passed to the executable file. In the case
+ * this variable will not be used.
+ * @av: The name of the program.
+ * Return: Always 0.
+ */
+int main(__attribute__((unused)) int ac, char **av)
+{
+	char *line;
+	size_t size;
+	int command_counter;
+
+	command_counter = 0;
+	signal(SIGINT, SIG_IGN);
+	do {
+		command_counter++;
+		line = NULL;
+		size = 0;
+		parse_line(line, size, command_counter, av);
+
+	} while (1);
+
+	return (0);
+}
